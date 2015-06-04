4250d5d916cd96e393eb3ae3f85c6553ef609312
fancymans <lindarrin@gmail.com>
2015-06-03 23:19:36 -0700

    Formatting and merge

--- sql/src/create_tables.sql
+++ sql/src/create_tables.sql
@@ -6,16 +6,16 @@ DROP TABLE USR;
 
 
 CREATE TABLE USR(
-	userId varchar(10) UNIQUE NOT NULL, 
-	password varchar(10) NOT NULL,
+	userId varchar(50) UNIQUE NOT NULL,
+	password varchar(50) NOT NULL,
 	email text NOT NULL,
 	name char(50),
 	dateOfBirth date,
 	Primary Key(userId));
 
 CREATE TABLE WORK_EXPR(
-	userId char(10) NOT NULL, 
-	company char(50) NOT NULL, 
+	userId char(50) NOT NULL,
+	company char(50) NOT NULL,
 	role char(50) NOT NULL,
 	location char(50),
 	startDate date,
@@ -23,8 +23,8 @@ CREATE TABLE WORK_EXPR(
 	PRIMARY KEY(userId,company,role,startDate));
 
 CREATE TABLE EDUCATIONAL_DETAILS(
-	userId char(10) NOT NULL, 
-	instituitionName char(50) NOT NULL, 
+	userId char(50) NOT NULL,
+	instituitionName char(50) NOT NULL,
 	major char(50) NOT NULL,
 	degree char(50) NOT NULL,
 	startdate date,
@@ -32,9 +32,9 @@ CREATE TABLE EDUCATIONAL_DETAILS(
 	PRIMARY KEY(userId,major,degree));
 
 CREATE TABLE MESSAGE(
-	msgId integer UNIQUE NOT NULL, 
-	senderId char(10) NOT NULL,
-	receiverId char(10) NOT NULL,
+	msgId integer UNIQUE NOT NULL,
+	senderId char(50) NOT NULL,
+	receiverId char(50) NOT NULL,
 	contents char(500) NOT NULL,
 	sendTime timestamp,
 	deleteStatus integer,
@@ -42,7 +42,7 @@ CREATE TABLE MESSAGE(
 	PRIMARY KEY(msgId));
 
 CREATE TABLE CONNECTION_USR(
-	userId char(10) NOT NULL, 
-	connectionId char(10) NOT NULL, 
+	userId char(50) NOT NULL,
+	connectionId char(50) NOT NULL,
 	status char(30) NOT NULL,
 	PRIMARY KEY(userId,connectionId));
--- sql/src/load_data.sql
+++ sql/src/load_data.sql
@@ -1,4 +1,24 @@
-DROP TABLE USR;
-CREATE TABLE USR
-(UserId text, Password text, email text, FullName text, dateofbirth text);
-COPY USR FROM '/home/csmajs/dtran042/Desktop/postgresql-proj/data/USR.csv' WITH DELIMITER ',' CSV;
+COPY CONNECTION_USR FROM '/Users/Lin/github/postgresql-proj/data/Connection.csv' WITH DELIMITER ',' CSV;
+
+COPY EDUCATIONAL_DETAILS FROM '/Users/Lin/github/postgresql-proj/data/Edu_Det.csv' WITH DELIMITER ',' CSV;
+
+COPY MESSAGE FROM '/Users/Lin/github/postgresql-proj/data/Message.csv' WITH DELIMITER ',' CSV;
+
+COPY USR FROM '/Users/Lin/github/postgresql-proj/data/USR.csv' WITH DELIMITER ',' CSV;
+
+COPY WORK_EXPR FROM '/Users/Lin/github/postgresql-proj/data/Work_Ex.csv' WITH DELIMITER ',' CSV;
+
+
+/*
+COPY USR FROM '/Users/Darren/Desktop/cs166/postgresql-proj/data/USR.csv' WITH DELIMITER ',' CSV;
+
+COPY CONNECTION_USR FROM '/Users/Darren/Desktop/cs166/postgresql-proj/data/Connection.csv' WITH DELIMITER ',' CSV;
+
+COPY EDUCATIONAL_DETAILS FROM '/Users/Darren/Desktop/cs166/postgresql-proj/data/Edu_Det.csv' WITH DELIMITER ',' CSV;
+
+COPY MESSAGE FROM '/Users/Darren/Desktop/cs166/postgresql-proj/data/Message.csv' WITH DELIMITER ',' CSV;
+
+COPY WORK_EXPR FROM '/Users/Darren/Desktop/cs166/postgresql-proj/data/Work_Ex.csv' WITH DELIMITER ',' CSV;
+*/
+
+
