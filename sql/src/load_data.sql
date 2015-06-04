77e1141e1ac310f9720a75d38713b8240e34c6b5
fancymans <lindarrin@gmail.com>
2015-06-03 23:54:03 -0700

    gitignore fix

--- sql/src/load_data.sql
+++ sql/src/load_data.sql
@@ -8,7 +8,7 @@ COPY USR FROM '/Users/Lin/github/postgresql-proj/data/USR.csv' WITH DELIMITER ',
 
 COPY WORK_EXPR FROM '/Users/Lin/github/postgresql-proj/data/Work_Ex.csv' WITH DELIMITER ',' CSV;
 
-/*
+
 COPY USR FROM '/Users/Darren/Desktop/cs166/postgresql-proj/data/USR.csv' WITH DELIMITER ',' CSV;
 
 COPY CONNECTION_USR FROM '/Users/Darren/Desktop/cs166/postgresql-proj/data/Connection.csv' WITH DELIMITER ',' CSV;
@@ -18,6 +18,6 @@ COPY EDUCATIONAL_DETAILS FROM '/Users/Darren/Desktop/cs166/postgresql-proj/data/
 COPY MESSAGE FROM '/Users/Darren/Desktop/cs166/postgresql-proj/data/Message.csv' WITH DELIMITER ',' CSV;
 
 COPY WORK_EXPR FROM '/Users/Darren/Desktop/cs166/postgresql-proj/data/Work_Ex.csv' WITH DELIMITER ',' CSV;
-*/
+
 
 
